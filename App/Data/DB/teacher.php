10eb2dc8a83e9737586e172ccbfa86531af09446
Alexander <alexander.semik.99@gmail.com>
2020-03-31 01:50:05 +0300

    Получение информации о преподавателе по кафедре и факультету

--- App/Data/DB/teacher.php
+++ App/Data/DB/teacher.php
@@ -20,4 +20,38 @@ class Teacher{
         }
         return $teacher_array;
     }
+
+    public static function get_teachers_in_faculty(Database $connection, $teacher_faculty_name){
+        $result = $connection->query('SELECT * FROM get_teachers_in_faculty('.$teacher_faculty_name.')');
+        while($row = pg_fetch_assoc($result)){
+            $teacher_array = [
+                'name' => 'name',
+                'second_name' => 'second_name',
+                'third_name' => 'third_name',
+                'login_teacher' => 'login_teacher',
+                'photo' => 'photo',
+                'faculty_id' => 'faculty_id',
+                'department_id' => 'department_id',
+                'position_id' => 'position_id',
+            ];
+        }
+        return $teacher_array;
+    }
+
+    public static function get_teachers_in_department(Database $connection, $teacher_faculty_name, $teacher_department_name){
+        $result = $connection->query('SELECT * FROM get_teachers_in_faculty('.$teacher_faculty_name.','.$teacher_department_name.')');
+        while($row = pg_fetch_assoc($result)){
+            $teacher_array = [
+                'name' => 'name',
+                'second_name' => 'second_name',
+                'third_name' => 'third_name',
+                'login_teacher' => 'login_teacher',
+                'photo' => 'photo',
+                'faculty_id' => 'faculty_id',
+                'department_id' => 'department_id',
+                'position_id' => 'position_id',
+            ];
+        }
+        return $teacher_array;
+    }
 }
