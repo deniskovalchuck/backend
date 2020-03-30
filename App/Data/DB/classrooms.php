c8ea2d43f24d168233185fd56a25eda6bc78d4d4
AlexanderSemik <semikalexander@yandex.ua>
2020-03-30 16:15:05 +0300

    Получение всех аудиторий и получение аудиторий в корпусе

--- App/Data/DB/classrooms.php
+++ App/Data/DB/classrooms.php
@@ -4,8 +4,21 @@ namespace App\Data\DB;
 use Core\Database\Database;
 
 class Classrooms{
+
     public static function get_all_bulding(Database $connection){
         $result =  $connection->query('SELECT * FROM get_all_housing()');
+        $arr = pg_fetch_all($result);
+        return $arr;
+    }
+
+    public static function get_all_classrooms(Database $connection){
+        $result =  $connection->query('SELECT * FROM get_all_classes()');
+        $arr = pg_fetch_all($result);
+        return $arr;
+    }
+
+    public static function get_all_classrooms_in_building(Database $connection, $num_building){
+        $result =  $connection->query('SELECT * FROM get_all_classes_in_building('.$num_building.')');
         $arr = pg_fetch_all($result);
         return $arr;
     }
