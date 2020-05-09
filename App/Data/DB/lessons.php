f6b8d9b2441bb945bdacb5815949d63fdf7551c7
Denis Kovalchuck <deniskovalchuck@yandex.ua>
2020-05-09 22:29:16 +0300

    Добавил Экранирование переменных

--- App/Data/DB/lessons.php
+++ App/Data/DB/lessons.php
@@ -9,21 +9,21 @@ use Core\Database\Database;
 class Lesson{
     /*return integer (-1 - error)*/
     public static function get_id_lesson(Database $connection, $name_input_type_lesson, $name_input_type_education, $name_input_payment_type, $num_input_lesson, $week_input_day, $week_input_type, $start_input_date, $end_input_date){
-        $result = $connection->query('SELECT * get_id_lesson('.$name_input_type_lesson.', '.$name_input_type_education.', '.$name_input_payment_type.', '.$num_input_lesson.', '.$week_input_day.', '.$week_input_type.', '.$start_input_date.', '.$end_input_date.')');
+        $result = $connection->query('SELECT * get_id_lesson(\''.$name_input_type_lesson.'\', \''.$name_input_type_education.'\', \''.$name_input_payment_type.'\', \''.$num_input_lesson.'\', \''.$week_input_day.'\', \''.$week_input_type.'\', \''.$start_input_date.'\', \''.$end_input_date.'\')');
         $arr = pg_fetch_all($result);
         return $arr;
     }
 
     /*return integer (-1 - error)*/
     public static function add_lesson(Database $connection, $name_input_type_lesson, $name_input_education_type, $name_input_payment_type, $week_input_type, $week_input_day, $num_input_lesson, $start_input_date, $end_input_date){
-        $result = $connection->query('SELECT * add_lesson('.$name_input_type_lesson.', '.$name_input_education_type.', '.$name_input_payment_type.', '.$week_input_type.', '.$week_input_day.', '.$num_input_lesson.', '.$start_input_date.', '.$end_input_date.')');
+        $result = $connection->query('SELECT * add_lesson(\''.$name_input_type_lesson.'\', \''.$name_input_education_type.', \''.$name_input_payment_type.'\', \''.$week_input_type.'\', \''.$week_input_day.'\', \''.$num_input_lesson.'\', \''.$start_input_date.'\', \''.$end_input_date.'\')');
         $arr = pg_fetch_all($result);
         return $arr;
     }
 
     /*return string ('Запись успешно удалена!', 'Занятия нет в базе!')*/
     public static function delete_lesson(Database $connection, $id_input_lesson){
-        $result = $connection->query('SELECT * delete_lesson('.$id_input_lesson.')');
+        $result = $connection->query('SELECT * delete_lesson(\''.$id_input_lesson.'\')');
         $arr = pg_fetch_all($result);
         return $arr;
     }
@@ -54,7 +54,7 @@ class Lesson{
     }
 
     public static function get_all_lessons_by_group(Database $connection, $abbrevation_input_group, $year_entry_input, $name_faculty_input, $name_department_input, $name_specialization_input, $education_type_input, $sub_input_group){
-        $result = $connection->query('SELECT * get_all_lessons_by_group('.$abbrevation_input_group.', '.$year_entry_input.', '.$name_faculty_input.', '.$name_department_input.', '.$name_specialization_input.', '.$education_type_input.', '.$sub_input_group.')');
+        $result = $connection->query('SELECT * get_all_lessons_by_group(\''.$abbrevation_input_group.'\', \''.$year_entry_input.', \''.$name_faculty_input.', \''.$name_department_input.'\', \''.$name_specialization_input.'\', \''.$education_type_input.'\', \''.$sub_input_group.'\')');
         $group_lessons_array = array();
         while($row = pg_fetch_assoc($result)){
             $group_lessons_array = [
@@ -70,7 +70,7 @@ class Lesson{
     }
 
     public static function get_all_lessons_by_teacher(Database $connection, $login_input_teacher){
-        $result = $connection->query('SELECT * get_all_lessons_by_teacher('.$login_input_teacher.')');
+        $result = $connection->query('SELECT * get_all_lessons_by_teacher(\''.$login_input_teacher.'\')');
         $teacher_lessons_array = array();
         while($row = pg_fetch_assoc($result)){
             $teacher_lessons_array = [
