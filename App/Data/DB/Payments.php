686fe05c29b8f8a0c8f505278308c21df284e34f
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2020-05-16 23:01:47 +0300

    Get functions for id in payment type table

--- App/Data/DB/Payments.php
+++ App/Data/DB/Payments.php
@@ -12,14 +12,34 @@ class Payments{
     }
 
     /*return string ('Запись успешно удалена!', 'Записи нет в базе!')*/
-    public static function delete_payment_type(Database $connection, $input_name_payment_type){
-        $result = $connection->query('SELECT * FROM delete_payment_type(\''.$input_name_payment_type.'\')');
+    public static function delete_payment_type(Database $connection, $input_name_payment_type, $input_coefficient){
+        $result = $connection->query('SELECT * FROM delete_payment_type(\''.$input_name_payment_type.'\','.$input_coefficient.')');
         $arr = pg_fetch_all($result);
         return $arr;
     }
 
     public static function get_all_payment_type(Database $connection){
         $result = $connection->query('SELECT * FROM get_all_payment_type()');
+        $payment_type_array = array();
+        $i=0;
+        while($row = pg_fetch_assoc($result)){
+            $payment_type_array[$i] = [
+                'name_input_payment_type' => $row['name_input_payment_type'],
+                'coefficient_input_payment_type' => $row['coefficient_input_payment_type'],
+            ];
+            $i++;
+        }
+        return $payment_type_array;
+    }
+
+    public static function get_payment_type_id(Database $connection, $input_name_payment_type, $input_coefficient){
+        $result = $connection->query('SELECT * FROM get_week_id(\''.$input_name_payment_type.'\','.$input_coefficient.')');
+        $arr = pg_fetch_all($result);
+        return $arr;
+    }
+
+    public static function get_payment_type_by_id(Database $connection, $id_input_payment_type){
+        $result = $connection->query('SELECT * FROM get_all_payment_type('.$id_input_payment_type.')');
         $payment_type_array = array();
         $i=0;
         while($row = pg_fetch_assoc($result)){
