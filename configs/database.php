8e3745084ad7631e68c8964b26f483f736718fcf
Denis Kovalchuck <deniskovalchuck@yandex.ua>
2020-06-23 23:01:15 +0300

    Update database.php

--- configs/database.php
+++ configs/database.php
@@ -10,8 +10,8 @@ return [
             'host' => 'server.myteh24.ru',
             'port' =>'5432',
             'database' => 'semik_univer',
-            'username' => 'semik_semikalex',//если пусто - ожидать от пользователя, иначе использовать этот
-            'password' => 'Shdenis21',//если пусто - ожидать от пользователя, иначе использовать этот
+            'username' => 'semik_semikalex',
+            'password' => 'Shdenis21',
             'charset' => 'utf8',
         ],
         
